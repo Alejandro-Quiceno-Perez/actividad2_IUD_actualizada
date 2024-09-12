97d4d9884b23a6adfe4e4753be9557f98074ca4f
Alejo Perez <alejo.perez2905@gmail.com>
2024-09-12 11:55:01 -0500

    Se inicia el proyecto y se enlaza a git

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('node:express');
+
+const app = express();
+const port = process.env.PORT || 9000;
+app.listen(port, () => console.log('Server Listening on port' , port));
+
+//  User: alejandroquiceno
+// Password: etolia5Z2URNI0Ej
+// IP: 177.255.144.191
