c84df03de25c36c3e4f5a0800cb7b78f652837c1
alejo <alejo.perez2905@gmail.com>
2024-09-12 12:49:28 -0500

    Se crea la variable de entorno

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-const express = require('node:express');
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 9000;
+
+//routes
+app.get('/', (req, res) => {
+       res.send('Hello World');
+})
+
+//mongo db conection 
+mongoose.connect('')
+
+
 app.listen(port, () => console.log('Server Listening on port' , port));
 
 //  User: alejandroquiceno
