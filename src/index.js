4522d00f68e2c36f58af2891a6879bd08adaca69
alejo <alejo.perez2905@gmail.com>
2024-09-13 13:30:42 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+// importamos todas las rutas de User
+const directorRouter = require('./routes/DirectorRouter');
+const generoRouter = require('./routes/GeneroRouter');
+// const mediaRouter = require('./routes/MediaRouter');
+const productoraRouter = require('./routes/ProductoraRouter');
+const tipoRouter = require('./routes/TipoRouter');
 
 const app = express();
 const port = process.env.PORT || 9000;
+
+// middelware
+app.use(express.json());
+//Middleware
+app.use('/api', directorRouter);
+app.use('/api', generoRouter);
+// app.use('/api', mediaRouter);
+app.use('/api', productoraRouter);
+app.use('/api', tipoRouter);
 
 //routes
 app.get('/', (req, res) => {
@@ -11,7 +26,9 @@ app.get('/', (req, res) => {
 })
 
 //mongo db conection 
-mongoose.connect('')
+mongoose.connect(process.env.ACTIVIDAD2_IUD_ACTUALIZADA)
+       .then(() => console.log('Conected mongoDB_Atlas'))
+       .catch((error) => console.error(error.message));
 
 
 app.listen(port, () => console.log('Server Listening on port' , port));
